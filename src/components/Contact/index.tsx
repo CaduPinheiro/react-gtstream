c7acc00df32526bdd8cae5b2dbeed709be0a308d
CaduPinheiro <99294607+CaduPinheiro@users.noreply.github.com>
2022-04-05 09:09:15 -0300

    Upgrade

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -9,12 +9,21 @@ export const Contact: React.FC<{}> = (props) => {
             </div>
             <div className='Info'>
                 <div className='SegueAi'>
-                    <h1>Segue aí!</h1>
+                    <h1>Quer saber mais?</h1>
                 </div>
                 <div className='Icons'>
-                    <a>Facebook: @GTStream</a>
-                    <a>Instagram: @GTStream</a>
-                    <a>Twitter: @GTStream</a>
+                    <div className='Icons--facebook'>
+                        { <img className='iconFacebook' src='https://openvisualfx.com/wp-content/uploads/2019/10/1509135364flat-facebook-logo-png-icon-circle.png' alt='imagem' />}
+                        <a>Facebook: @GTStream</a>
+                    </div>
+                    <div className='Icons--instagram'>
+                        { <img className='iconInstagram' src='http://blog.delupo.com.br/wp-content/uploads/2017/12/174590_5164106c214b4eb7bae96fd762fdc6af-mv2.png' alt='imagem' />}
+                        <a>Instagram: @GTStream</a>
+                    </div>
+                    <div className='Icons--twitter'>
+                        { <img className='iconTwitter' src='https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png' alt='imagem' />}
+                        <a>Twitter: @GTStream</a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,10 @@
 import './Home.css';
-import { Header } from '../Header'
 
 export const Home: React.FC<{}> = (props) => {
     return (
         <div className='Home'>
             <h1>Tudo o que você quer assistir</h1>
-            <h2>Por apenas R$ 29,99 por mês</h2>
+            <h2>Por apenas R$ 29,90 por mês</h2>
             <div className='Home--btn'>
                 <a>Assine já!</a>
             </div>
